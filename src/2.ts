45778a3c04b82df5e0bd24c3ae19f9f59619f9a8
AlinaS13 <soulofangel13@gmail.com>
2023-12-16 15:14:50 +0200

    add hw-3

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,12 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -32,8 +33,13 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
+const manager = new Manager("name", "department", 2000);
+manager.getEmployeeDetails();
 
 export {};
